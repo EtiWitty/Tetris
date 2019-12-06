02e38ce7e68d897f5f2ef44854e3ad324661aa90
EtiWitty <atiyeh.keshavarz@gmail.com>
2019-12-05 19:56:58 -0800

    initiated App file

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { component } from 'react';
+class App extends Comoponent{
+	render() {
+		return(
+			<div>
+				<h1> Hello World!</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
